3ab9a28171853bd59159ee11cbfac1cadf96d4ad
catastrophi <wlzang2@gmail.com>
2026-04-03 16:34:13 -0400

    part 1 - basic app setup

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+const helmet = require('helmet');
+
+const router = require('./router.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const dbURI = process.send.MONGODB_URI || 'mmongodb://localgost/DomoMaker';
+mongoose.connect(dbURI).catch((err) => {
+    if(err) {
+        console.log('Could not connect to database');
+        throw err;
+    }
+});
+
